721c597a44f87ab9223e2d30461d7725c2120854
CameronDC <31133230+CameronDC@users.noreply.github.com>
2017-08-18 14:28:10 +0100

    Primary backbone
    setting up the game

--- Project.py
+++ Project.py
@@ -1,1 +1,33 @@
-s
+import pygame
+
+pygame.init()
+
+RED = (255,0,0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+screen= pygame.display.set_mode((640,480))
+
+clock = pygame.time.Clock()
+
+pygame.display.set_caption("Name")
+
+done = False
+
+while not done:
+
+    #Inputs
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+
+            done = True
+
+    screen.fill(COLOUR)
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
+
