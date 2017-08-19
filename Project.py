15d8baaf597e56ff041440730ad34b7529e870bc
CameronDC <31133230+CameronDC@users.noreply.github.com>
2017-08-19 09:07:25 +0100

    commenting stage 1
    comments complete for player and player movement

--- Project.py
+++ Project.py
@@ -2,61 +2,71 @@ import pygame
 
 pygame.init()
 
-RED = (255,0,0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-GRAY = ( 128, 128, 128)
+RED = (255,0,0)               #Definign Colours
+GREEN = (0, 255, 0)           #       v
+BLUE = (0, 0, 255)            #       v
+GRAY = ( 128, 128, 128)       #----------------
 
-playerlist = pygame.sprite.Group()
+playerlist = pygame.sprite.Group() #putting player in its own list
 screen= pygame.display.set_mode((640,480))#Screen size
 keypress = pygame.key.get_pressed() 
-rol = 0
-uod = 0
-clock = pygame.time.Clock()
+rol = 0 #right or left setting to 0
+uod = 0 # setting up or down to 0
+clock = pygame.time.Clock() #taking the pygame clock
 
-##Classes##
+#==============================================================================================
+#--------------------------------------------Classes-------------------------------------------
+#==============================================================================================
+
 class Player(pygame.sprite.Sprite):
         def __init__(self, health, zero, damage):
-            self.health = health
-            self.velocityx = zero
-            self.velocityy = zero
-            self.damage = damage
+            self.health = health        #Creating attributes of PLAYER to be used later in game
+            self.velocityx = zero       #                         v
+            self.velocityy = zero       #                         v
+            self.damage = damage        #------------------------------------------------------
             super().__init__()
-            self.image = pygame.image.load("N.png")
+            self.image = pygame.image.load("N.png") #Load default image for player
             self.rect = self.image.get_rect()
 
         def facing(rol, uod):
-            if keypress[pygame.K_RIGHT]:
-                if keypress[pygame.K_UP]:
-                    player.image = pygame.image.load("NE.png")
-                elif keypress[pygame.K_DOWN]:
-                    player.image = pygame.image.load("SE.png")
-                else:
-                    player.image = pygame.image.load("E.png")
-            elif keypress[pygame.K_LEFT]:
-                if keypress[pygame.K_UP]:
-                    player.image = pygame.image.load("NW.png")
-                elif keypress[pygame.K_DOWN]:
-                    player.image = pygame.image.load("SW.png")
-                else:
-                    player.image = pygame.image.load("W.png")
-            elif keypress[pygame.K_DOWN]:
-                player.image = pygame.image.load("S.png")
-            else:
-                player.image = pygame.image.load("N.png")
+            if keypress[pygame.K_RIGHT]: #Determining the durection of user (If Right AND....
+                if keypress[pygame.K_UP]: #If Right and up 
+                    player.image = pygame.image.load("NE.png") #Change player image to NE.png
+                elif keypress[pygame.K_DOWN]: #If right and down
+                    player.image = pygame.image.load("SE.png") #Change player image to SE.png
+                else: #If it isnt up or down or left then it must be right which is East
+                    player.image = pygame.image.load("E.png") #change player image to E.png
+            elif keypress[pygame.K_LEFT]: #Same system with the left side of the compass
+                if keypress[pygame.K_UP]: #If left and up
+                    player.image = pygame.image.load("NW.png") #Change player image to NW.png
+                elif keypress[pygame.K_DOWN]: #If left and down
+                    player.image = pygame.image.load("SW.png") #Change player image to SW.png
+                else: #If it isnt up or down or right then it must be left which is West
+                    player.image = pygame.image.load("W.png") #Change player image to W.png
+            elif keypress[pygame.K_DOWN]:                  #Is it north or south and changing
+                player.image = pygame.image.load("S.png")  #              Image
+             else:                                         #                v
+                player.image = pygame.image.load("N.png")  #---------------------------------
                 
-        def moving():
+        def moving(): #Moving function consisting of direction + the determined velocity.
             player.rect.centerx = player.rect.centerx + player.velocityx
             player.rect.centery = player.rect.centery + player.velocityy
 
+#===============================================================================================
+#------------------------------Variable setting/code before loop--------------------------------
+#===============================================================================================
+
 for counter in range(1):
-    player = Player(100, 0, 10)
-    player.rect.centerx = 137
-    player.rect.centery = 137
+    player = Player(100, 0, 10) #Setting Players Health and samage
+    player.rect.centerx = 137 #Setting players start X co-ordinate
+    player.rect.centery = 137 #Setting players start Y co-ordinate
     playerlist.add(player)
 
-pygame.display.set_caption("Game")
+pygame.display.set_caption("Game") #Setting caption at top to Game
 
+#===============================================================================================
+#------------------------------------------Game Loop--------------------------------------------
+#===============================================================================================
 done = False
 
 while not done:
@@ -72,26 +82,26 @@ while not done:
 
     
         
-    if keypress[pygame.K_LEFT]:
-        player.velocityx = - 1
-        rol = 2
-        for player in playerlist:
+    if keypress[pygame.K_LEFT]:      # if left hey is pressed
+        player.velocityx = - 1       # X velocity set to go backwards
+        rol = 2                      # rol value set to 2 to be used later
+        for player in playerlist:    
             Player.facing(rol, uod)
-    elif keypress[pygame.K_RIGHT]:
-        player.velocityx = 1
-        rol = 1
+    elif keypress[pygame.K_RIGHT]:   # if right key is pressed
+        player.velocityx = 1         # X velocity is set to go forewards 1
+        rol = 1                      # rol value set to 1
         for player in playerlist:
             Player.facing(rol, uod)
     
         
-    if keypress[pygame.K_UP]:
-        player.velocityy = - 1
-        uod = 1
+    if keypress[pygame.K_UP]:        # if up key is pressed 
+        player.velocityy = - 1       # Y velocity set to go down 1 which is up in pygame
+        uod = 1                      # uod value set to 2
         for player in playerlist:
             Player.facing(rol, uod)
-    elif keypress[pygame.K_DOWN]:
-        player.velocityy = 1
-        uod = 2
+    elif keypress[pygame.K_DOWN]:    # if down key is pressed
+        player.velocityy = 1         # Y velocity set to go up 1 which is down in pyagme
+        uod = 2                      # uod value set to 2
         for player in playerlist:
             Player.facing(rol, uod)
     
@@ -104,10 +114,10 @@ while not done:
         
     keypress = pygame.key.get_pressed()        
    
-    screen.fill(GRAY)
-    playerlist.draw(screen)
-    player.velocityx = 0
-    player.velocityy = 0
+    screen.fill(GRAY)               # Screen fill GRAY
+    playerlist.draw(screen)         # draw the player onto pf the screen
+    player.velocityx = 0            # default x velocity = 0
+    player.velocityy = 0            # default y velocity = 0
     pygame.display.flip()
 
     clock.tick(60)
